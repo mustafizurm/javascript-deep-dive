e1a0c404b8d022c61d185543c0166f11c8d9415d
Mustafizur <mustafizurrahman1256@gmail.com>
2026-03-31 13:17:42 +0600

    file name updated from FunctionIntro to Function_intro

---
+++ funtions/Function_Intro.js
@@ -0,0 +1,35 @@
+// what is function
+
+// Functions are Code Blocks
+// Functions are reusable code blocks designed to perform a particular task.
+// Functions are executed when they are called or invoked.
+// Functions are fundamental in all programming languages.
+
+// why we use function
+
+// Functions help us:
+// Reuse code (write once, run many times)
+// Organize code into smaller parts
+// Make code easier to read and maintain
+
+// Why Functions?
+
+// Functions enable better code organization and efficiency.
+// With functions you can reuse the same code many times.
+// The same code, with different input, can produce different results.
+
+// What Does a Function Look Like?
+
+// A function can be created with the function keyword, a name, and parentheses.
+// The code to run is written inside curly brackets.
+function run(){
+
+}
+
+// example
+function sum(num1, num2){
+    return num1 + num2
+}
+sum(3,4)
+sum(6,8)
+sum(10,20)
